2d560028be235f0f8151f8fcf91ddced13f849f5
Richie McIlroy <33632126+richiemcilroy@users.noreply.github.com>
2023-11-27 20:18:54 +0000

    Temp fix, adding text-black manually

--- apps/landing/components/Footer.tsx
+++ apps/landing/components/Footer.tsx
@@ -2,7 +2,7 @@ export const Footer = () => {
   return (
     <footer className="py-4 border-t">
       <div className="wrapper text-center">
-        <p className="text-xs">© Copyright 2023 Cap.</p>
+        <p className="text-xs text-black">© Copyright 2023 Cap.</p>
       </div>
     </footer>
   );
