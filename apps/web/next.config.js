c769143d9af4929b32fbbd59b4b4d98eccc64988
Richie McIlroy <33632126+richiemcilroy@users.noreply.github.com>
2024-01-11 17:12:36 +0000

    fix: Remove requirement for SUPABASE_SERVICE_ROLE_KEY

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -4,8 +4,7 @@ require("dotenv").config({ path: "../../.env" });
 
 if (
   !process.env.NEXT_PUBLIC_SUPABASE_URL ||
-  !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
-  !process.env.SUPABASE_SERVICE_ROLE_KEY
+  !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 ) {
   throw new Error("Please set Supabase env vars in .env file");
 }
