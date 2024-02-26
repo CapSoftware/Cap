5cc4a206208ea4ce3ac2a1d13535de01de983ad1
eroist <97012368+eroist@users.noreply.github.com>
2024-02-25 21:53:17 -0500

    feat: add record page

---
+++ apps/desktop/src/pages/record.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import Record from "@/views/record";
+
+const RecordPage: NextPage = () => {
+  return <Record />;
+};
+
+export default RecordPage;
--- apps/desktop/src/views/main/index.tsx
+++ apps/desktop/src/views/main/index.tsx
@@ -115,6 +115,17 @@ const Home: NextPage = () => {
               </button>
             </Link>
           </div>
+
+          <div className="flex items-center gap-5 mt-5">
+            <Link href="/record">
+              <button
+                type="button"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+              >
+                record video
+              </button>
+            </Link>
+          </div>
         </div>
       </section>
     </Page>
