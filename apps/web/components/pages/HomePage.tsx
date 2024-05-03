732e7b8d4350f228c7a271b07dd0dcf16411f59e
Richie McIlroy <33632126+richiemcilroy@users.noreply.github.com>
2024-05-03 15:56:01 +0100

    feat: Add cap public beta banner

--- apps/web/components/pages/HomePage.tsx
+++ apps/web/components/pages/HomePage.tsx
@@ -16,16 +16,22 @@ export const HomePage = () => {
           <div className="wrapper wrapper-sm mx-auto">
             <div className="mb-auto -mt-20 text-center md:px-4 pt-40 pb-52 md:pb-64 space-y-8">
               <Link
-                href="https://www.producthunt.com/posts/cap-3?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-cap-3"
+                href="/updates/cap-public-beta-launch"
                 target="_blank"
+                className="mx-auto inline-flex justify-center text-sm font-medium transition-colors focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50 border bg-tertiary hover:bg-transparent h-9 px-4 py-2 rounded-full border-border space-x-2 items-center"
               >
-                <img
-                  src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=452581&theme=light"
-                  alt="Cap - Open Source Loom Alternative | Product Hunt"
-                  style={{ width: 250, height: 54, margin: "0 auto" }}
-                  width={250}
-                  height={54}
-                />
+                <span>Announcing Cap Public Beta</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width={12}
+                  height={12}
+                  fill="none"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M8.783 6.667H.667V5.333h8.116L5.05 1.6 6 .667 11.333 6 6 11.333l-.95-.933 3.733-3.733Z"
+                  />
+                </svg>
               </Link>
 
               <h1 className="fade-in-down text-3xl lg:text-6xl relative z-10 text-black">
