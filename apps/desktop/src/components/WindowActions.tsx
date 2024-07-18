8d506f1c83701ae9534e0e11d410a5f23e722b15
Johnpaul <polymenjohn1@gmail.com>
2024-07-18 13:47:12 +0100

    chore: add settings button to window actions

--- apps/desktop/src/components/WindowActions.tsx
+++ apps/desktop/src/components/WindowActions.tsx
@@ -1,17 +1,19 @@
 "use client";
 
-import { exit } from "@tauri-apps/api/process";
+import { openLinkInBrowser, openSettingsWindow } from "@/utils/helpers";
+
 import { Home } from "@/components/icons/Home";
-import { openLinkInBrowser } from "@/utils/helpers";
+import { Settings } from "@/components/icons/Settings";
+import { exit } from "@tauri-apps/api/process";
 
 export const WindowActions = () => {
   const actionButtonBase = "w-3 h-3 bg-gray-500 rounded-full m-0 p-0 block";
   const tauriWindow = import("@tauri-apps/api/window");
 
   return (
-    <div className="w-full flex items-center -mt-3 z-20 absolute top-5">
-      <div className="flex flex-grow items-center justify-between px-3">
-        <div className="flex space-x-2">
+    <div className='w-full flex items-center -mt-3 z-20 absolute top-5'>
+      <div className='flex flex-grow items-center justify-between px-3'>
+        <div className='flex space-x-2'>
           <div>
             <button
               onClick={async () => {
@@ -20,8 +22,7 @@ export const WindowActions = () => {
                 }
                 await exit();
               }}
-              className={`bg-red-500 hover:bg-red-700 transition-all ${actionButtonBase}`}
-            ></button>
+              className={`bg-red-500 hover:bg-red-700 transition-all ${actionButtonBase}`}></button>
           </div>
           <div>
             <button
@@ -36,14 +37,13 @@ export const WindowActions = () => {
                   }
                 });
               }}
-              className={`bg-orange-400 hover:bg-orange-500 transition-all ${actionButtonBase}`}
-            ></button>
+              className={`bg-orange-400 hover:bg-orange-500 transition-all ${actionButtonBase}`}></button>
           </div>
           <div>
             <span className={actionButtonBase}></span>
           </div>
         </div>
-        <div className="flex">
+        <div className='flex'>
           <button
             onClick={async () => {
               if (window.fathom !== undefined) {
@@ -53,13 +53,17 @@ export const WindowActions = () => {
                 `${process.env.NEXT_PUBLIC_URL}/dashboard`
               );
             }}
-            className="p-1.5 bg-transparent hover:bg-gray-200 rounded-full transition-all"
-          >
-            <Home className="w-5 h-5" />
+            className='p-1.5 bg-transparent hover:bg-gray-200 rounded-full transition-all'>
+            <Home className='w-5 h-5' />
           </button>
-          {/* <button className="p-1.5 bg-transparent hover:bg-gray-200 rounded-full transition-all">
-            <Settings className="w-5 h-5" />
-          </button> */}
+
+          <button
+            onClick={async () => {
+              openSettingsWindow();
+            }}
+            className='p-1.5 bg-transparent hover:bg-gray-200 rounded-full transition-all'>
+            <Settings className='w-5 h-5' />
+          </button>
         </div>
       </div>
     </div>
