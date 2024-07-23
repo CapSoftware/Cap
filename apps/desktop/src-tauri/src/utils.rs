59e10dd4439086c52f0b676da007645cc4c26401
Brendan Allan <brendonovich@outlook.com>
2024-07-23 16:50:11 +0800

    remove references to 'capture' crate

--- apps/desktop/src-tauri/src/utils.rs
+++ apps/desktop/src-tauri/src/utils.rs
@@ -1,74 +1,11 @@
-use capture::{Capturer, Display};
 use ffmpeg_sidecar::paths::sidecar_dir;
-use std::io::ErrorKind::WouldBlock;
-use std::panic;
 use std::path::Path;
 use std::process::Command;
-use std::thread;
-use std::time::{Duration, Instant};
 
 #[tauri::command]
 #[specta::specta]
 pub fn has_screen_capture_access() -> bool {
-    let display = match Display::primary() {
-        Ok(display) => display,
-        Err(_) => return false,
-    };
-
-    let width = display.width();
-    let height = display.height();
-    let one_second = Duration::new(1, 0);
-    let one_frame = one_second / 60;
-
-    println!("width: {}", width);
-    println!("height: {}", height);
-
-    let result = panic::catch_unwind(|| {
-        let mut capturer = match Capturer::new(display, width, height) {
-            Ok(capturer) => {
-                println!("Capturer created");
-                capturer
-            }
-            Err(e) => {
-                println!("Capturer not created: {}", e);
-                return false;
-            }
-        };
-
-        println!("Capturer created");
-
-        let start = Instant::now();
-
-        loop {
-            if start.elapsed() > Duration::from_secs(2) {
-                println!("Loop exited");
-                return false;
-            }
-
-            match capturer.frame() {
-                Ok(_frame) => {
-                    println!("Frame captured");
-                    return true;
-                }
-                Err(error) => {
-                    if error.kind() == WouldBlock {
-                        thread::sleep(one_frame);
-                        continue;
-                    } else {
-                        println!("Error: {}", error);
-                        return false;
-                    }
-                }
-            };
-        }
-    });
-
-    println!("Result: {:?}", result);
-
-    match result {
-        Ok(val) => val,
-        Err(_) => false,
-    }
+    scap::has_permission()
 }
 
 pub fn run_command(command: &str, args: Vec<&str>) -> Result<(String, String), String> {
