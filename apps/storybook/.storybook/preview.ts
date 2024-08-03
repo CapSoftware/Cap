3f2df01c33649785348d753b1863815a12942ee3
Brendan Allan <brendonovich@outlook.com>
2024-08-03 16:26:11 +0800

    add storybook for solidjs

---
+++ apps/storybook/.storybook/preview.ts
@@ -0,0 +1,14 @@
+import "@cap/ui/style";
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
