9f2fb20c07f0fc8d49872581bd368abe8e30a7cc
Richie McIlroy <33632126+richiemcilroy@users.noreply.github.com>
2024-09-06 14:04:47 +0100

    feat: Remove old desktop folder

--- apps/desktop/.eslintrc.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: [require.resolve("config/eslint/web.js")],
-  parserOptions: {
-    tsconfigRootDir: __dirname,
-    project: "./tsconfig.json",
-  },
-};
--- apps/desktop/src/components/windows/Options.tsx
+++
@@ -1,5 +0,0 @@
-"use client";
-import { SignIn } from "@/components/windows/inner/SignIn";
-export const Options = () => {
-  return <SignIn />;
-};
