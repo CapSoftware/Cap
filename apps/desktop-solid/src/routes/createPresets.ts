0253da28b6fc8c8ca1413aa2920f828b8966983d
Brendan Allan <brendonovich@outlook.com>
2024-09-10 00:59:20 +0800

    cleanup presets

--- apps/desktop-solid/src/routes/createPresets.ts
+++ apps/desktop-solid/src/routes/createPresets.ts
@@ -1,7 +1,7 @@
 import { createResource, onCleanup } from "solid-js";
+import { Store } from "@tauri-apps/plugin-store";
 
 import type { ProjectConfiguration } from "../utils/tauri";
-import { Store } from "@tauri-apps/plugin-store";
 
 const store = new Store("frontend-stuff");
 
@@ -34,54 +34,54 @@ export function createPresets() {
   );
   onCleanup(() => cleanup()?.());
 
+  async function updatePresets(fn: (prev: PresetsStore) => PresetsStore) {
+    const p = query();
+    if (!p) throw new Error("Presets not loaded");
+
+    const newValue = fn(p);
+
+    await store.set("presets", newValue);
+    await store.save();
+  }
+
   return {
     query,
-    createPreset: async (preset: CreatePreset) => {
-      const p = query();
-      if (!p) throw new Error("Presets not loaded");
+    createPreset: (preset: CreatePreset) =>
+      updatePresets((prev) => ({
+        presets: [
+          ...prev.presets,
+          { name: preset.name, config: preset.config },
+        ],
+        default: preset.default ? prev.presets.length : prev.default,
+      })),
+    deletePreset: (index: number) =>
+      updatePresets((prev) => {
+        prev.presets.splice(index, 1);
 
-      await store.set("presets", {
-        presets: [...p.presets, { name: preset.name, config: preset.config }],
-        default: preset.default ? p.presets.length : p.default,
-      });
-    },
-    deletePreset: async (index: number) => {
-      const p = query();
-      if (!p) throw new Error("Presets not loaded");
-
-      p.presets.splice(index, 1);
-
-      await store.set("presets", {
-        presets: p.presets,
-        default:
-          index > p.presets.length - 1 ? p.presets.length - 1 : p.default,
-      });
-    },
+        return {
+          presets: prev.presets,
+          default:
+            index > prev.presets.length - 1
+              ? prev.presets.length - 1
+              : prev.default,
+        };
+      }),
     getDefaultConfig: () => {
       const p = query();
       if (!p) return;
 
       return p.presets[p.default ?? 0].config;
     },
-    setDefault: async (index: number) => {
-      const p = query();
-      if (!p) throw new Error("Presets not loaded");
-
-      await store.set("presets", {
-        presets: [...p.presets],
+    setDefault: (index: number) =>
+      updatePresets((prev) => ({
+        presets: [...prev.presets],
         default: index,
-      });
-    },
-    renamePreset: async (index: number, name: string) => {
-      const p = query();
-      if (!p) throw new Error("Presets not loaded");
+      })),
+    renamePreset: (index: number, name: string) =>
+      updatePresets((prev) => {
+        prev.presets[index].name = name;
 
-      p.presets[index].name = name;
-
-      await store.set("presets", {
-        presets: p.presets,
-        default: p.default,
-      });
-    },
+        return prev;
+      }),
   };
 }
