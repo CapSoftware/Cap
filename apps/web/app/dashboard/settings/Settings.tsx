c6b9066bd5a23405d61f7d6908f378ec5d001066
Richie McIlroy <33632126+richiemcilroy@users.noreply.github.com>
2024-09-27 20:14:12 +0200

    fix: Onboarding inputs

--- apps/web/app/dashboard/settings/Settings.tsx
+++ apps/web/app/dashboard/settings/Settings.tsx
@@ -65,7 +65,7 @@ export const Settings = ({
         </CardHeader>
         <CardContent>
           <div className="space-y-3">
-            <div>
+            <div className="flex flex-col items-center">
               <Label htmlFor="firstName">First name</Label>
               <Input
                 type="text"
@@ -74,7 +74,7 @@ export const Settings = ({
                 name="firstName"
               />
             </div>
-            <div>
+            <div className="flex flex-col items-center">
               <Label htmlFor="lastName">Last name</Label>
               <Input
                 type="text"
