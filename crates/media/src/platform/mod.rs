4d68e1e71d2655a30ab7ce3d6192c35c5c059121
Ilya <47112191+ItsEeleeya@users.noreply.github.com>
2024-10-15 22:33:41 +0330

    Declare media functions for Windows platform

--- crates/media/src/platform/mod.rs
+++ crates/media/src/platform/mod.rs
@@ -5,6 +5,10 @@ use specta::Type;
 #[path = "macos.rs"]
 mod platform_impl;
 
+#[cfg(target_os = "windows")]
+#[path = "windows.rs"]
+mod platform_impl;
+
 pub use platform_impl::*;
 
 #[derive(Debug, Clone, Copy, Serialize, Deserialize, Type)]
---
+++ crates/media/src/platform/windows.rs
@@ -0,0 +1,11 @@
+use crate::platform::{Bounds, Window};
+
+pub fn get_on_screen_windows() -> Vec<Window> {
+    // TODO!
+    vec![]
+}
+
+pub fn bring_window_to_focus(window_id: u32) {
+    // TODO!
+    println!("Bring {window_id} to focus")
+}
