79ec0b8a5a165566a4f9480c066399d2e6d144e8
Ilya <47112191+ItsEeleeya@users.noreply.github.com>
2024-10-17 02:31:16 +0330

    Fix nokhwa features in Cargo.toml

--- crates/media/src/platform/windows.rs
+++ crates/media/src/platform/windows.rs
@@ -1,7 +1,16 @@
 use crate::platform::{Bounds, Window};
+use serde::{Deserialize, Serialize};
+use windows::Win32::Foundation::{HWND, LPARAM, RECT};
+use windows::Win32::UI::WindowsAndMessaging::{
+    EnumWindows, GetWindowRect, GetWindowTextW, GetWindowThreadProcessId, IsWindowVisible,
+};
+
+unsafe extern "system" fn enum_window_proc(hwnd: HWND, lparam: LPARAM) -> i32 {
+    let windows = &mut *(lparam.0 as *mut Vec<Window>);
+    1
+}
 
 pub fn get_on_screen_windows() -> Vec<Window> {
-    // TODO!
     vec![]
 }
 
