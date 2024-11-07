ab1762b9ed84f2773d02f9e288367eceec4cd46e
Brendan Allan <brendonovich@outlook.com>
2024-11-08 02:06:11 +0800

    remove hw encoding for video

--- crates/ffmpeg-hw-device/src/lib.rs
+++ crates/ffmpeg-hw-device/src/lib.rs
@@ -125,35 +125,35 @@ impl CodecContextExt for codec::decoder::decoder::Decoder {
     }
 }
 
-impl CodecContextExt for codec::encoder::video::Video {
-    fn try_use_hw_device(
-        &mut self,
-        device_type: AVHWDeviceType,
-        pix_fmt: Pixel,
-    ) -> Result<HwDevice, &'static str> {
-        unsafe {
-            let mut hw_device_ctx = null_mut();
-            if av_hwdevice_ctx_create(&mut hw_device_ctx, device_type, null(), null_mut(), 0) < 0 {
-                return Err("failed to create hw device context");
-            }
+// impl CodecContextExt for codec::encoder::video::Video {
+//     fn try_use_hw_device(
+//         &mut self,
+//         device_type: AVHWDeviceType,
+//         pix_fmt: Pixel,
+//     ) -> Result<HwDevice, &'static str> {
+//         unsafe {
+//             let mut hw_device_ctx = null_mut();
+//             if av_hwdevice_ctx_create(&mut hw_device_ctx, device_type, null(), null_mut(), 0) < 0 {
+//                 return Err("failed to create hw device context");
+//             }
 
-            let hw_frames_ref = av_hwframe_ctx_alloc(hw_device_ctx);
-            let frames_ctx = (*hw_frames_ref).data as *mut AVHWFramesContext;
-            (*frames_ctx).format = self.format().into();
-            (*frames_ctx).sw_format = AVPixelFormat::AV_PIX_FMT_NV12;
-            (*frames_ctx).width = self.width() as i32;
-            (*frames_ctx).height = self.height() as i32;
-            (*frames_ctx).initial_pool_size = 20;
+//             let hw_frames_ref = av_hwframe_ctx_alloc(hw_device_ctx);
+//             let frames_ctx = (*hw_frames_ref).data as *mut AVHWFramesContext;
+//             (*frames_ctx).format = self.format().into();
+//             (*frames_ctx).sw_format = AVPixelFormat::AV_PIX_FMT_NV12;
+//             (*frames_ctx).width = self.width() as i32;
+//             (*frames_ctx).height = self.height() as i32;
+//             (*frames_ctx).initial_pool_size = 20;
 
-            av_hwframe_ctx_init(hw_frames_ref);
+//             av_hwframe_ctx_init(hw_frames_ref);
 
-            (*self.as_mut().as_mut_ptr()).hw_frames_ctx = av_buffer_ref(hw_frames_ref);
+//             (*self.as_mut().as_mut_ptr()).hw_frames_ctx = av_buffer_ref(hw_frames_ref);
 
-            Ok(HwDevice {
-                device_type,
-                ctx: hw_device_ctx,
-                pix_fmt,
-            })
-        }
-    }
-}
+//             Ok(HwDevice {
+//                 device_type,
+//                 ctx: hw_device_ctx,
+//                 pix_fmt,
+//             })
+//         }
+//     }
+// }
