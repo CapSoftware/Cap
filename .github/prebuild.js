00b430e4066557c22e54a305983a619ad35a8033
Brendan Allan <brendonovich@outlook.com>
2024-11-13 19:45:04 +0800

    libavfilter

--- .github/prebuild.js
+++ .github/prebuild.js
@@ -12,6 +12,88 @@ const keychain = env.APPLE_KEYCHAIN ? `--keychain ${env.APPLE_KEYCHAIN}` : "";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const __root = path.resolve(path.join(__dirname, ".."));
+const nativeDeps = path.join(__root, "native-deps");
+
+async function main() {
+  await fs.mkdir(path.join(__root, ".cargo"), { recursive: true });
+
+  await fs.writeFile(
+    path.join(__root, ".cargo/config.toml"),
+    `[env]
+FFMPEG_DIR = { force = true, value = "${nativeDeps}" }
+
+[target.x86_64-apple-darwin]
+rustflags = [
+"-L",
+"${nativeDeps}/lib",
+"-Csplit-debuginfo=unpacked",
+]
+
+[target.aarch64-apple-darwin]
+rustflags = [
+"-L",
+"${nativeDeps}/lib",
+"-Csplit-debuginfo=unpacked",
+]`
+  );
+
+  const os = process.argv[2];
+  const arch = process.argv[3];
+
+  if (!os) throw new Error("OS not provided");
+  if (!arch) throw new Error("Arch not provided");
+
+  await fs.rm(nativeDeps, { recursive: true, force: true });
+  await fs.mkdir(nativeDeps, { recursive: true });
+  const res = await fetch(`${NATIVE_DEPS_URL}/${NATIVE_DEPS_ASSETS[os][arch]}`);
+  const body = await res.blob();
+
+  if (os === "darwin") {
+    await fs.writeFile(
+      `${__root}/native-deps.tar.xz`,
+      Buffer.from(await body.arrayBuffer())
+    );
+    await exec(`tar xf ${__root}/native-deps.tar.xz -C ${nativeDeps}`);
+
+    await symlinkSharedLibsMacOS(nativeDeps).catch((e) => {
+      console.error(`Failed to symlink shared libs.`);
+      throw e;
+    });
+
+    await fs.writeFile(
+      `${__root}/apps/desktop/src-tauri/tauri.macos.conf.json`,
+      JSON.stringify(
+        {
+          bundle: {
+            macOS: {
+              frameworks: [path.join(nativeDeps, "Spacedrive.framework")],
+            },
+          },
+        },
+        null,
+        4
+      )
+    );
+  }
+}
+
+main();
+
+const NATIVE_DEPS_URL =
+  "https://github.com/spacedriveapp/native-deps/releases/latest/download";
+
+const NATIVE_DEPS_ASSETS = {
+  darwin: {
+    x86_64: "native-deps-x86_64-darwin-apple.tar.xz",
+    aarch64: "native-deps-aarch64-darwin-apple.tar.xz",
+  },
+  windows: {
+    x86_64: "native-deps-x86_64-windows-gnu.tar.xz ",
+    aarch64: "native-deps-aarch64-windows-gnu.tar.xz",
+  },
+};
+
 async function symlinkSharedLibsMacOS(nativeDeps) {
   // Framework
   const framework = path.join(nativeDeps, "Spacedrive.framework");
@@ -37,80 +119,3 @@ async function symlinkSharedLibsMacOS(nativeDeps) {
       )
     );
 }
-
-const __root = path.resolve(path.join(__dirname, ".."));
-const nativeDeps = path.join(__root, "native-deps");
-
-async function main() {
-  await fs.mkdir(path.join(__root, ".cargo"), { recursive: true });
-
-  await fs.writeFile(
-    path.join(__root, ".cargo/config.toml"),
-    `
-[env]
-FFMPEG_DIR = { force = true, value = "${nativeDeps}" }
-
-[target.x86_64-apple-darwin]
-rustflags = [
-"-L",
-"${nativeDeps}/lib",
-"-Csplit-debuginfo=unpacked",
-]
-
-[target.aarch64-apple-darwin]
-rustflags = [
-"-L",
-"${nativeDeps}/lib",
-"-Csplit-debuginfo=unpacked",
-]`
-  );
-
-  await fs.rm(nativeDeps, { recursive: true, force: true });
-  await fs.mkdir(nativeDeps, { recursive: true });
-  const res = await fetch(
-    `${NATIVE_DEPS_URL}/${NATIVE_DEPS_ASSETS[process.argv[2]][process.argv[3]]}`
-  );
-  const body = await res.blob();
-
-  await fs.writeFile(
-    `${__root}/native-deps.tar.xz`,
-    Buffer.from(await body.arrayBuffer())
-  );
-  await exec(`tar xf ${__root}/native-deps.tar.xz -C ${nativeDeps}`);
-
-  await symlinkSharedLibsMacOS(nativeDeps).catch((e) => {
-    console.error(`Failed to symlink shared libs.`);
-    throw e;
-  });
-
-  await fs.writeFile(
-    `${__root}/apps/desktop/src-tauri/tauri.macos.conf.json`,
-    JSON.stringify(
-      {
-        bundle: {
-          macOS: {
-            frameworks: [path.join(nativeDeps, "Spacedrive.framework")],
-          },
-        },
-      },
-      null,
-      4
-    )
-  );
-}
-
-main();
-
-const NATIVE_DEPS_URL =
-  "https://github.com/spacedriveapp/native-deps/releases/latest/download";
-
-const NATIVE_DEPS_ASSETS = {
-  darwin: {
-    x86_64: "native-deps-x86_64-darwin-apple.tar.xz",
-    aarch64: "native-deps-aarch64-darwin-apple.tar.xz",
-  },
-  windows: {
-    x86_64: "native-deps-x86_64-windows-gnu.tar.xz ",
-    aarch64: "native-deps-aarch64-windows-gnu.tar.xz",
-  },
-};
