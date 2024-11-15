8df1667fd40364727765b927ac52eee6d42dc3e4
Ilya <47112191+ItsEeleeya@users.noreply.github.com>
2024-11-16 00:19:01 +0330

    Actually stop the terminal opening when rendering on Windows

--- crates/rendering/src/main.rs
+++ crates/rendering/src/main.rs
@@ -1,5 +1,3 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 // use cap_project::{BackgroundSource, ProjectConfiguration, RecordingMeta};
 
 #[tokio::main]
