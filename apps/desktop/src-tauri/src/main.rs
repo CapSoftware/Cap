a7519bc93f2bba10730bff66f9ab309fc170619d
Ilya <47112191+ItsEeleeya@users.noreply.github.com>
2024-11-18 04:31:25 +0330

    Make sentry optional

--- apps/desktop/src-tauri/src/main.rs
+++ apps/desktop/src-tauri/src/main.rs
@@ -3,29 +3,41 @@
 
 use std::sync::Arc;
 
-#[tokio::main]
-async fn main() {
-    let _guard = sentry::init((
-        env!("CAP_DESKTOP_SENTRY_URL"),
-        sentry::ClientOptions {
-            release: sentry::release_name!(),
-            debug: cfg!(debug_assertions),
-            before_send: Some(Arc::new(|mut event| {
-                #[cfg(debug_assertions)]
-                return None;
+fn main() {
+    // We have to hold onto the ClientInitGuard until the very end
+    let _guard = match std::env::var("CAP_DESKTOP_SENTRY_URL") {
+        Ok(sentry_url) => Some(sentry::init((
+            sentry_url,
+            sentry::ClientOptions {
+                release: sentry::release_name!(),
+                debug: cfg!(debug_assertions),
+                before_send: Some(Arc::new(|event| {
+                    #[cfg(debug_assertions)]
+                    {
+                        let msg = event.message.clone().unwrap_or("No message".into());
+                        println!("Sentry captured {}: {}", &event.level, &msg);
+                        Some(event)
+                    }
 
-                #[cfg(not(debug_assertions))]
-                {
-                    Some(event)
-                }
-            })),
-            ..Default::default()
-        },
-    ));
+                    #[cfg(not(debug_assertions))]
+                    {
+                        Some(event)
+                    }
+                })),
+                ..Default::default()
+            },
+        ))),
+        Err(_) => {
+            tracing::warn!(
+                "Sentry URL not found in environment variables, skipping Sentry initialization"
+            );
+            None
+        }
+    };
 
     tokio::runtime::Builder::new_multi_thread()
         .enable_all()
         .build()
         .expect("Failed to build multi threaded tokio runtime")
-        .block_on(desktop_solid_lib::run);
+        .block_on(desktop_solid_lib::run());
 }
