605f85ee72d5856e16a0c6e014abefa204edbf0b
Ilya <47112191+ItsEeleeya@users.noreply.github.com>
2024-11-20 01:37:12 +0330

    Sentry client must be initialized

--- apps/desktop/src-tauri/src/main.rs
+++ apps/desktop/src-tauri/src/main.rs
@@ -5,38 +5,30 @@ use std::sync::Arc;
 
 fn main() {
     // We have to hold onto the ClientInitGuard until the very end
-    let _guard = match std::env::var("CAP_DESKTOP_SENTRY_URL") {
-        Ok(sentry_url) => Some(sentry::init((
-            sentry_url,
-            sentry::ClientOptions {
-                release: sentry::release_name!(),
-                debug: cfg!(debug_assertions),
-                before_send: Some(Arc::new(|event| {
-                    #[cfg(debug_assertions)]
-                    {
-                        let msg = event.message.clone().unwrap_or("No message".into());
-                        println!("Sentry captured {}: {}", &event.level, &msg);
-                        println!("-- user: {:?}", &event.user);
-                        println!("-- event tags: {:?}", &event.tags);
-                        println!("-- event contexts: {:?}", &event.contexts);
-                        None
-                    }
+    let _guard = sentry::init((
+        dotenvy_macro::dotenv!("CAP_DESKTOP_SENTRY_URL"),
+        sentry::ClientOptions {
+            release: sentry::release_name!(),
+            debug: cfg!(debug_assertions),
+            before_send: Some(Arc::new(|event| {
+                #[cfg(debug_assertions)]
+                {
+                    let msg = event.message.clone().unwrap_or("No message".into());
+                    println!("Sentry captured {}: {}", &event.level, &msg);
+                    println!("-- user: {:?}", &event.user);
+                    println!("-- event tags: {:?}", &event.tags);
+                    println!("-- event contexts: {:?}", &event.contexts);
+                    None
+                }
 
-                    #[cfg(not(debug_assertions))]
-                    {
-                        Some(event)
-                    }
-                })),
-                ..Default::default()
-            },
-        ))),
-        Err(_) => {
-            eprintln!(
-                "Sentry URL not found in environment variables, skipping Sentry initialization"
-            );
-            None
-        }
-    };
+                #[cfg(not(debug_assertions))]
+                {
+                    Some(event)
+                }
+            })),
+            ..Default::default()
+        },
+    ));
 
     #[cfg(debug_assertions)]
     sentry::configure_scope(|scope| {
