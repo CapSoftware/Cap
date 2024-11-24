1e3ea16ef2d9884d92ab54f85b649796668373ea
Richie McIlroy <33632126+richiemcilroy@users.noreply.github.com>
2024-11-24 14:00:47 +0000

    feat: add dark mode logo

--- apps/desktop/src/routes/(window-chrome)/(main).tsx
+++ apps/desktop/src/routes/(window-chrome)/(main).tsx
@@ -121,7 +121,8 @@ export default function () {
     <div class="flex justify-center flex-col p-[1rem] gap-[0.75rem] text-[0.875rem] font-[400] bg-[--gray-50] h-full text-[--text-primary]">
       <div class="flex items-center justify-between pb-[0.25rem]">
         <div class="flex items-center space-x-1">
-          <IconCapLogoFull class="w-[90px] h-auto text-[--text-primary]" />
+          <IconCapLogoFullDark class="w-[90px] h-auto text-[--text-primary] dark:block hidden" />
+          <IconCapLogoFull class="w-[90px] h-auto text-[--text-primary] dark:hidden block" />
           <span
             onClick={async () => {
               if (!isUpgraded()) {
