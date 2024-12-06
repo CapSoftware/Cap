c8925b50bddb181edb2d874a4e0f13472d260dd5
Brendan Allan <brendonovich@outlook.com>
2024-12-06 15:00:45 +0800

    look in target-specific release folder

--- apps/desktop/scripts/stripDebugSymbols.js
+++ apps/desktop/scripts/stripDebugSymbols.js
@@ -6,18 +6,33 @@ import { promisify } from "node:util";
 
 const exec = promisify(execCb);
 
+console.log(process.env);
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const targetDir = path.join(__dirname, "../../../target");
-const releaseDir = path.join(targetDir, "release");
 
 async function main() {
+  const dirs = [];
+  const releaseDir = path.join(targetDir, "release");
   const releaseFiles = await fs.readdir(releaseDir);
-  const binaryPath = path.join(
-    releaseDir,
-    releaseFiles.find((f) => f.startsWith("Cap"))
-  );
+  let releaseFile = releaseFiles.find((f) => f.startsWith("Cap"));
+  dirs.push(releaseDir);
+
+  if (!releaseFile) {
+    const releaseDir = path.join(
+      targetDir,
+      `release/${process.env.TAURI_ENV_TARGET_TRIPLE}`
+    );
+    dirs.push(releaseDir);
+    const releaseFiles = await fs.readdir(releaseDir);
+    releaseFile = releaseFiles.find((f) => f.startsWith("Cap"));
+  }
+
+  if (!releaseFile) throw new Error(`No binary found at ${dirs.join(", ")}`);
+
+  const binaryPath = path.join(releaseDir, releaseFile);
 
   await exec(`dsymutil "${binaryPath}" -o "${binaryPath}.dSYM"`);
   await exec(`strip "${binaryPath}"`);
