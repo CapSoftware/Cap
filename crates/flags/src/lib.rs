f89b79e970d2e88cadb6ccea282ba9a0be9e1b1c
Richie McIlroy <33632126+richiemcilroy@users.noreply.github.com>
2025-01-17 15:19:55 +0000

    Change flag

--- crates/flags/src/lib.rs
+++ crates/flags/src/lib.rs
@@ -11,5 +11,5 @@ pub const FLAGS: Flags = Flags {
     record_mouse: true,
     split: false,        // cfg!(debug_assertions),
     pause_resume: false, // cfg!(debug_assertions),
-    zoom: cfg!(debug_assertions),
+    zoom: true,
 };
