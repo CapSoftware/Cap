763c610f0693d38bff9255169b315c0cf7aba916
Brendan Allan <brendonovich@outlook.com>
2025-03-07 19:07:34 +0800

    fix logs for instant recording

--- crates/recording/src/instant_recording.rs
+++ crates/recording/src/instant_recording.rs
@@ -137,233 +137,188 @@ pub async fn spawn_instant_recording_actor(
     audio_input_feed: Option<AudioInputFeed>,
 ) -> Result<(InstantRecordingHandle, tokio::sync::oneshot::Receiver<()>), RecordingError> {
     ensure_dir(&recording_dir)?;
-    let logfile = File::create(recording_dir.join("recording-logs.log"))?;
-
-    let collector = tracing_subscriber::registry()
-        .with(
-            tracing_subscriber::fmt::layer()
-                .with_ansi(false)
-                .with_target(false)
-                .with_writer(logfile)
-                .with_filter(
-                    tracing_subscriber::filter::EnvFilter::builder()
-                        .with_default_directive(tracing::level_filters::LevelFilter::TRACE.into())
-                        .from_env_lossy(),
-                ),
-        )
-        .with(
-            tracing_subscriber::fmt::layer()
-                .with_ansi(true)
-                .with_target(false)
-                .with_filter(
-                    tracing_subscriber::filter::EnvFilter::builder()
-                        .with_default_directive(tracing::level_filters::LevelFilter::TRACE.into())
-                        .from_env_lossy(),
-                ),
-        );
 
     let (done_tx, done_rx) = tokio::sync::oneshot::channel::<()>();
 
-    async {
-        async {
-            let run = async || {
-                trace!("creating recording actor");
+    trace!("creating recording actor");
 
-                let content_dir = ensure_dir(&recording_dir.join("content"))?;
+    let content_dir = ensure_dir(&recording_dir.join("content"))?;
 
-                let screen_source = create_screen_capture(&options.capture_target, true, true, 30)?;
+    let screen_source = create_screen_capture(&options.capture_target, true, true, 30)?;
 
-                debug!("screen capture: {screen_source:#?}");
+    debug!("screen capture: {screen_source:#?}");
 
-                if let Some(audio_feed) = &audio_input_feed {
-                    debug!("mic audio info: {:#?}", audio_feed.audio_info())
-                }
+    if let Some(audio_feed) = &audio_input_feed {
+        debug!("mic audio info: {:#?}", audio_feed.audio_info())
+    }
 
-                let (pipeline, pipeline_done_rx) = create_pipeline(
-                    content_dir.join("output.mp4"),
-                    screen_source.clone(),
-                    audio_input_feed.as_ref(),
-                )
-                .await?;
+    let (pipeline, pipeline_done_rx) = create_pipeline(
+        content_dir.join("output.mp4"),
+        screen_source.clone(),
+        audio_input_feed.as_ref(),
+    )
+    .await?;
 
-                let segment_start_time = current_time_f64();
+    let segment_start_time = current_time_f64();
 
-                let (ctrl_tx, ctrl_rx) = flume::bounded(1);
+    let (ctrl_tx, ctrl_rx) = flume::bounded(1);
 
-                trace!("spawning recording actor");
+    trace!("spawning recording actor");
 
-                spawn_actor({
-                    let options = options.clone();
-                    let video_info = screen_source.info();
-                    async move {
-                        let mut actor = InstantRecordingActor {
-                            id,
-                            recording_dir,
-                            capture_target: options.capture_target,
-                            video_info,
-                            audio_input_name: options.audio_input_name,
-                        };
-
-                        let mut state = InstantRecordingActorState::Recording {
-                            pipeline,
-                            pipeline_done_rx,
-                            segment_start_time,
-                        };
-
-                        'outer: loop {
-                            state = match state {
-                                InstantRecordingActorState::Recording {
-                                    mut pipeline,
-                                    mut pipeline_done_rx,
-                                    segment_start_time,
-                                } => {
-                                    info!("recording actor recording");
-                                    loop {
-                                        let msg = tokio::select! {
-                                            _ = &mut pipeline_done_rx => {
-                                                break 'outer;
-                                            }
-                                            msg = ctrl_rx.recv_async() => {
-                                                let Ok(msg) = msg else {
-                                                    break 'outer;
-                                                };
-
-                                                info!("received control message: {msg:?}");
-
-                                                msg
-                                            }
-                                        };
-
-                                        async fn shutdown(
-                                            mut pipeline: InstantRecordingPipeline,
-                                            actor: &mut InstantRecordingActor,
-                                            segment_start_time: f64,
-                                        ) -> Result<(), RecordingError>
-                                        {
-                                            pipeline.inner.shutdown().await?;
-                                            Ok(())
-                                        }
-
-                                        break match msg {
-                                            InstantRecordingActorControlMessage::Pause(tx) => {
-                                                InstantRecordingActorState::Recording {
-                                                    pipeline,
-                                                    pipeline_done_rx,
-                                                    segment_start_time,
-                                                }
-                                                // let res = shutdown(
-                                                //     pipeline,
-                                                //     &mut actor,
-                                                //     segment_start_time,
-                                                // )
-                                                // .await;
-
-                                                // tx.send(res.map_err(Into::into)).ok();
-                                                // InstantRecordingActorState::Paused
-                                            }
-                                            InstantRecordingActorControlMessage::Stop(tx) => {
-                                                let res = shutdown(
-                                                    pipeline,
-                                                    &mut actor,
-                                                    segment_start_time,
-                                                )
-                                                .await;
-                                                let res = match res {
-                                                    Ok(_) => stop_recording(actor).await,
-                                                    Err(e) => Err(e),
-                                                };
-
-                                                tx.send(res).ok();
-
-                                                break 'outer;
-                                            }
-                                            _ => continue,
-                                        };
-                                    }
-                                }
-                                InstantRecordingActorState::Paused => {
-                                    info!("recording actor paused");
-                                    loop {
-                                        let Ok(msg) = ctrl_rx.recv_async().await else {
-                                            break 'outer;
-                                        };
-
-                                        break match msg {
-                                            InstantRecordingActorControlMessage::Stop(tx) => {
-                                                tx.send(stop_recording(actor).await).ok();
-                                                break 'outer;
-                                            }
-                                            InstantRecordingActorControlMessage::Resume(tx) => {
-                                                // let (state, res) = match create_pipeline(
-                                                //     &segments_dir,
-                                                //     &cursors_dir,
-                                                //     next_index,
-                                                //     screen_source.clone(),
-                                                //     camera_feed.as_deref(),
-                                                //     audio_input_feed.as_ref(),
-                                                //     cursors,
-                                                //     next_cursor_id,
-                                                // )
-                                                // .await
-                                                // {
-                                                //     Ok((pipeline, pipeline_done_rx)) => (
-                                                //         InstantRecordingActorState::Recording {
-                                                //             pipeline,
-                                                //             pipeline_done_rx,
-                                                //             index: next_index,
-                                                //             segment_start_time: current_time_f64(),
-                                                //         },
-                                                //         Ok(()),
-                                                //     ),
-                                                //     Err(e) => (
-                                                //         InstantRecordingActorState::Stopped,
-                                                //         Err(e.into()),
-                                                //     ),
-                                                // };
-
-                                                // tx.send(res).ok();
-
-                                                state
-                                            }
-                                            _ => continue,
-                                        };
-                                    }
-                                }
-                                InstantRecordingActorState::Stopped => {
-                                    info!("recording actor paused");
-                                    break;
-                                }
-                            };
-                        }
-
-                        info!("recording actor finished");
-
-                        done_tx.send(()).ok();
-                    }
-                    .in_current_span()
-                });
-
-                Ok(InstantRecordingHandle {
-                    ctrl_tx,
-                    options,
-                    bounds: screen_source.get_bounds().clone(),
-                })
+    spawn_actor({
+        let options = options.clone();
+        let video_info = screen_source.info();
+        async move {
+            let mut actor = InstantRecordingActor {
+                id,
+                recording_dir,
+                capture_target: options.capture_target,
+                video_info,
+                audio_input_name: options.audio_input_name,
             };
 
-            match run().await {
-                Ok(a) => Ok(a),
-                Err(e) => {
-                    error!("Failed to start recording actor: {}", e);
-                    Err(e)
-                }
+            let mut state = InstantRecordingActorState::Recording {
+                pipeline,
+                pipeline_done_rx,
+                segment_start_time,
+            };
+
+            'outer: loop {
+                state = match state {
+                    InstantRecordingActorState::Recording {
+                        mut pipeline,
+                        mut pipeline_done_rx,
+                        segment_start_time,
+                    } => {
+                        info!("recording actor recording");
+                        loop {
+                            let msg = tokio::select! {
+                                _ = &mut pipeline_done_rx => {
+                                    break 'outer;
+                                }
+                                msg = ctrl_rx.recv_async() => {
+                                    let Ok(msg) = msg else {
+                                        break 'outer;
+                                    };
+
+                                    info!("received control message: {msg:?}");
+
+                                    msg
+                                }
+                            };
+
+                            async fn shutdown(
+                                mut pipeline: InstantRecordingPipeline,
+                                actor: &mut InstantRecordingActor,
+                                segment_start_time: f64,
+                            ) -> Result<(), RecordingError> {
+                                pipeline.inner.shutdown().await?;
+                                Ok(())
+                            }
+
+                            break match msg {
+                                InstantRecordingActorControlMessage::Pause(tx) => {
+                                    InstantRecordingActorState::Recording {
+                                        pipeline,
+                                        pipeline_done_rx,
+                                        segment_start_time,
+                                    }
+                                    // let res = shutdown(
+                                    //     pipeline,
+                                    //     &mut actor,
+                                    //     segment_start_time,
+                                    // )
+                                    // .await;
+
+                                    // tx.send(res.map_err(Into::into)).ok();
+                                    // InstantRecordingActorState::Paused
+                                }
+                                InstantRecordingActorControlMessage::Stop(tx) => {
+                                    let res =
+                                        shutdown(pipeline, &mut actor, segment_start_time).await;
+                                    let res = match res {
+                                        Ok(_) => stop_recording(actor).await,
+                                        Err(e) => Err(e),
+                                    };
+
+                                    tx.send(res).ok();
+
+                                    break 'outer;
+                                }
+                                _ => continue,
+                            };
+                        }
+                    }
+                    InstantRecordingActorState::Paused => {
+                        info!("recording actor paused");
+                        loop {
+                            let Ok(msg) = ctrl_rx.recv_async().await else {
+                                break 'outer;
+                            };
+
+                            break match msg {
+                                InstantRecordingActorControlMessage::Stop(tx) => {
+                                    tx.send(stop_recording(actor).await).ok();
+                                    break 'outer;
+                                }
+                                InstantRecordingActorControlMessage::Resume(tx) => {
+                                    // let (state, res) = match create_pipeline(
+                                    //     &segments_dir,
+                                    //     &cursors_dir,
+                                    //     next_index,
+                                    //     screen_source.clone(),
+                                    //     camera_feed.as_deref(),
+                                    //     audio_input_feed.as_ref(),
+                                    //     cursors,
+                                    //     next_cursor_id,
+                                    // )
+                                    // .await
+                                    // {
+                                    //     Ok((pipeline, pipeline_done_rx)) => (
+                                    //         InstantRecordingActorState::Recording {
+                                    //             pipeline,
+                                    //             pipeline_done_rx,
+                                    //             index: next_index,
+                                    //             segment_start_time: current_time_f64(),
+                                    //         },
+                                    //         Ok(()),
+                                    //     ),
+                                    //     Err(e) => (
+                                    //         InstantRecordingActorState::Stopped,
+                                    //         Err(e.into()),
+                                    //     ),
+                                    // };
+
+                                    // tx.send(res).ok();
+
+                                    state
+                                }
+                                _ => continue,
+                            };
+                        }
+                    }
+                    InstantRecordingActorState::Stopped => {
+                        info!("recording actor paused");
+                        break;
+                    }
+                };
             }
+
+            info!("recording actor finished");
+
+            done_tx.send(()).ok();
         }
-        .instrument(tracing::info_span!("recording"))
-        .await
-    }
-    .with_subscriber(collector)
-    .await
-    .map(|a| (a, done_rx))
+        .in_current_span()
+    });
+
+    Ok((
+        InstantRecordingHandle {
+            ctrl_tx,
+            options,
+            bounds: screen_source.get_bounds().clone(),
+        },
+        done_rx,
+    ))
 }
 
 async fn stop_recording(
