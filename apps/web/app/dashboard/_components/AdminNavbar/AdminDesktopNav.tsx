edc761c13f1945b768ec10e853e0a00b8472cfc7
ameer2468 <33054370+ameer2468@users.noreply.github.com>
2025-04-15 12:49:42 +0300

    padding

--- apps/web/app/dashboard/_components/AdminNavbar/AdminDesktopNav.tsx
+++ apps/web/app/dashboard/_components/AdminNavbar/AdminDesktopNav.tsx
@@ -9,7 +9,7 @@ export const AdminDesktopNav = () => {
       <div className="hidden h-full lg:flex lg:flex-shrink-0 group">
         <div className="flex flex-col w-[200px]">
           <div className="flex overflow-y-auto flex-col flex-grow h-full">
-            <div className="flex flex-col flex-shrink-0 items-start px-4 h-full justify-top">
+            <div className="flex flex-col flex-shrink-0 items-start px-5 h-full justify-top">
               <div className="flex justify-center w-full h-[10vh] items-center">
                 <Link href="/dashboard">
                   <Logo className="w-auto h-9" />
