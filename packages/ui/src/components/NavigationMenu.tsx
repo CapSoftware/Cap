6c5dbc4c7eedca3826e2422097141ef4bd380979
Ameer Al Ashhab <33054370+ameer2468@users.noreply.github.com>
2025-04-29 07:20:13 +0300

    Update NavigationMenu.tsx (#487)

--- packages/ui/src/components/NavigationMenu.tsx
+++ packages/ui/src/components/NavigationMenu.tsx
@@ -73,7 +73,7 @@ const NavigationMenuContent = React.forwardRef<
   <NavigationMenuPrimitive.Content
     ref={ref}
     className={classNames(
-      "bg-gray-1/80 backdrop-blur-md left-0 top-0 w-full data-[motion^=from-]:animate-in data-[motion^=to-]:animate-out data-[motion^=from-]:fade-in data-[motion^=to-]:fade-out data-[motion=from-end]:slide-in-from-right-52 data-[motion=from-start]:slide-in-from-left-52 data-[motion=to-end]:slide-out-to-right-52 data-[motion=to-start]:slide-out-to-left-52 md:absolute md:w-auto shadow-lg rounded-lg border border-zinc-100/50",
+      "bg-white left-0 top-0 w-full data-[motion^=from-]:animate-in data-[motion^=to-]:animate-out data-[motion^=from-]:fade-in data-[motion^=to-]:fade-out data-[motion=from-end]:slide-in-from-right-52 data-[motion=from-start]:slide-in-from-left-52 data-[motion=to-end]:slide-out-to-right-52 data-[motion=to-start]:slide-out-to-left-52 md:absolute md:w-auto shadow-lg rounded-lg border border-zinc-100/50",
       className
     )}
     {...props}
@@ -129,7 +129,7 @@ const ListItem = React.forwardRef<
         <a
           ref={ref}
           className={classNames(
-            "block p-3 space-y-1 leading-none no-underline rounded-md transition-all duration-200 outline-none select-none hover:bg-zinc-50 hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            "block p-3 space-y-1 leading-none no-underline rounded-md transition-all duration-200 outline-none select-none hover:bg-gray-2 hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
             className
           )}
           {...props}
