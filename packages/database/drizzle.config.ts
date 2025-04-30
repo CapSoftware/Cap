73505f3e52933b00bf82461baec5aa149dd0c341
Richie McIlroy <33632126+richiemcilroy@users.noreply.github.com>
2025-04-29 18:31:51 -0700

    feat: Drizzle updates + migration folder (fresh)

--- packages/database/drizzle.config.ts
+++ packages/database/drizzle.config.ts
@@ -3,18 +3,26 @@ import type { Config } from "drizzle-kit";
 
 const URL = serverEnv.DATABASE_MIGRATION_URL ?? serverEnv.DATABASE_URL;
 
-if (!URL)
-  throw new Error("DATABASE_URL or DATABASE_MIGRATION_URL must be set!");
-if (!URL?.startsWith("mysql://"))
-  throw new Error(
-    "DATABASE_URL must be a 'mysql://' URI. Drizzle Kit doesn't support the fetch adapter!"
-  );
+// if (!URL)
+//   throw new Error("DATABASE_URL or DATABASE_MIGRATION_URL must be set!");
+// if (!URL?.startsWith("mysql://"))
+//   throw new Error(
+//     "DATABASE_URL must be a 'mysql://' URI. Drizzle Kit doesn't support the fetch adapter!"
+//   );
+
+// export default {
+//   schema: "./schema.ts",
+//   dbCredentials: {
+//     uri: URL,
+//   },
+//   out: "./migrations",
+//   driver: "mysql2",
+// } satisfies Config;
 
 export default {
   schema: "./schema.ts",
-  dbCredentials: {
-    uri: URL,
-  },
   out: "./migrations",
-  driver: "mysql2",
+  dialect: "mysql",
+  dbCredentials: { url: URL },
+  casing: "snake_case",
 } satisfies Config;
