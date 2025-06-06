4b0307a90c9fac1af1a55a8fe7b6ecd24b204628
Brendan Allan <brendonovich@outlook.com>
2025-06-06 18:09:30 +0800

    force dynamic

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -31,6 +31,8 @@ export const metadata: Metadata = {
   },
 };
 
+export const dynamic = "force-dynamic";
+
 export default async function RootLayout({ children }: PropsWithChildren) {
   const user = await getCurrentUser();
   const bootstrapData = await getBootstrapData();
