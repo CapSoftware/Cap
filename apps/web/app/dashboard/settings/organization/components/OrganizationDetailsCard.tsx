d9f59bb0a56008ea2086feceee50b80d1c147054
ameer2468 <33054370+ameer2468@users.noreply.github.com>
2025-06-11 13:59:45 +0300

    Update OrganizationDetailsCard.tsx

--- apps/web/app/dashboard/settings/organization/components/OrganizationDetailsCard.tsx
+++ apps/web/app/dashboard/settings/organization/components/OrganizationDetailsCard.tsx
@@ -45,8 +45,8 @@ export const OrganizationDetailsCard = ({
           <div className="space-y-1">
             <Label htmlFor="allowedEmailDomain">Access email domain</Label>
             <p className="text-sm text-gray-10">
-              Only emails from this domain can access shared videos. <br />
-              <span className="font-medium text-gray-11 leading-[0px]">
+              Only emails from this domain can access shared videos.{" "}
+              <span className="font-medium text-sm text-gray-11 leading-[0px]">
                 Leave blank to allow everyone.
               </span>
             </p>
