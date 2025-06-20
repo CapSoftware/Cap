2d81760e030691fe876817ddc062cbc18947862f
ameer2468 <33054370+ameer2468@users.noreply.github.com>
2025-06-20 15:13:48 +0300

    fix docs

--- apps/web/app/(site)/layout.tsx
+++ apps/web/app/(site)/layout.tsx
@@ -1,6 +1,6 @@
+import { PropsWithChildren } from "react";
 import { Footer } from "./Footer";
 import { Navbar } from "./Navbar";
-import { PropsWithChildren } from "react";
 
 export default function Layout(props: PropsWithChildren) {
   return (
