4bfe8b7fa6bce4f2932297c8fc985bd5f177f4d3
Adarsh Mamgain <52105807+adarsh-mamgain@users.noreply.github.com>
2025-07-09 02:21:15 +0530

    fix: ensure Navbar is always above on SeoPageTemplate pages and update background gradient on SelfHostingPage (#736)

--- apps/web/app/(site)/Navbar.tsx
+++ apps/web/app/(site)/Navbar.tsx
@@ -102,7 +102,7 @@ export const Navbar = () => {
 
   return (
     <>
-      <header className="sticky inset-0 top-4 z-[51] md:top-10  animate-in fade-in slide-in-from-top-4 duration-500">
+      <header className="fixed inset-0 top-4 z-[51] md:top-10  animate-in fade-in slide-in-from-top-4 duration-500">
         <nav className="p-2 mx-auto w-full max-w-[calc(100%-20px)] bg-white rounded-full border backdrop-blur-md md:max-w-fit border-zinc-200 h-fit">
           <div className="flex gap-12 justify-between items-center mx-auto max-w-4xl h-full transition-all">
             <div className="flex items-center">
