4a3f46d392110fe80d5562ea8bc0cde198fc244a
ameer2468 <33054370+ameer2468@users.noreply.github.com>
2025-07-16 19:05:26 +0300

    test embed on home

--- apps/web/components/pages/HomePage/RecordingModes.tsx
+++ apps/web/components/pages/HomePage/RecordingModes.tsx
@@ -97,7 +97,7 @@ const RecordingModes = () => {
               }}
             >
               <iframe
-                src="https://cap.so/embed/8cq21vmz12tm1zf"
+                src="https://cap-web-git-rework-video-player-mc-ilroy.vercel.app/embed/8cq21vmz12tm1zf"
                 frameBorder="0"
                 allowFullScreen
                 style={{
