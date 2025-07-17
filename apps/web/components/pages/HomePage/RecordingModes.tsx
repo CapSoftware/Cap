59c82caf3ff931cf5c2bb3107e50bbf1b1b2f2d9
ameer2468 <33054370+ameer2468@users.noreply.github.com>
2025-07-17 19:10:57 +0300

    Update RecordingModes.tsx

--- apps/web/components/pages/HomePage/RecordingModes.tsx
+++ apps/web/components/pages/HomePage/RecordingModes.tsx
@@ -99,8 +99,7 @@ const RecordingModes = () => {
               }}
             >
               <iframe
-                // src="https://cap-web-git-rework-video-player-mc-ilroy.vercel.app/embed/8cq21vmz12tm1zf"
-                src="http://localhost:3000/embed/vz2tdacm9yjja65"
+                src="https://cap-web-git-rework-video-player-mc-ilroy.vercel.app/embed/8cq21vmz12tm1zf"
                 frameBorder="0"
                 allowFullScreen
                 style={{
