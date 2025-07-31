680c1306eb6c577a3d3f41c3d3de513f19b9b1e3
Oscar Beaumont <oscar@otbeaumont.me>
2025-07-31 21:40:03 +0800

    big brain composite mode calculation

--- apps/desktop/src-tauri/src/camera.rs
+++ apps/desktop/src-tauri/src/camera.rs
@@ -498,13 +498,33 @@ impl Renderer {
             cache: None,
         });
 
+        let surface_capabilities = surface.get_capabilities(&adapter);
+        let alpha_mode = if surface_capabilities
+            .alpha_modes
+            .contains(&CompositeAlphaMode::PreMultiplied)
+        {
+            CompositeAlphaMode::PreMultiplied
+        } else if surface_capabilities
+            .alpha_modes
+            .contains(&CompositeAlphaMode::PostMultiplied)
+        {
+            CompositeAlphaMode::PostMultiplied
+        } else if surface_capabilities
+            .alpha_modes
+            .contains(&CompositeAlphaMode::PostMultiplied)
+        {
+            CompositeAlphaMode::PostMultiplied
+        } else {
+            CompositeAlphaMode::Inherit
+        };
+
         let surface_config = wgpu::SurfaceConfiguration {
             usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
             format: swapchain_format,
             width: size.width,
             height: size.height,
             present_mode: wgpu::PresentMode::Fifo,
-            alpha_mode: CompositeAlphaMode::PostMultiplied,
+            alpha_mode,
             view_formats: vec![],
             desired_maximum_frame_latency: 2,
         };
