14cfbc69c4d9cb6e1cc3290e5faba10d5b63a103
Brendan Allan <brendonovich@outlook.com>
2025-08-06 16:23:37 +0800

    formatting

--- crates/editor/examples/cli.rs
+++ crates/editor/examples/cli.rs
@@ -6,7 +6,7 @@ use std::path::PathBuf;
 async fn main() {
     let path: PathBuf = std::env::args().collect::<Vec<_>>().swap_remove(1).into();
 
-    let editor = EditorInstance::new(path,|s| {}).await.unwrap();
+    let editor = EditorInstance::new(path, |s| {}).await.unwrap();
 
     editor.start_playback(30, XY::new(1920, 1080)).await;
 
