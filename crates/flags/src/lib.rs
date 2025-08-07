47a2c54787c3bbdd161e51cc42e62e960605cadb
Oscar Beaumont <oscar@otbeaumont.me>
2025-08-07 15:22:45 +0800

    remove flag

--- crates/flags/src/lib.rs
+++ crates/flags/src/lib.rs
@@ -2,10 +2,8 @@
 #[serde(rename_all = "camelCase")]
 pub struct Flags {
     pub captions: bool,
-    pub new_recording_flow: bool,
 }
 
 pub const FLAGS: Flags = Flags {
     captions: false, // cfg!(debug_assertions),
-    new_recording_flow: cfg!(debug_assertions),
 };
