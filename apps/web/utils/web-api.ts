fc2552f78bba1c2f00a72313ed25802dc87eb5eb
Brendan Allan <brendonovich@outlook.com>
2025-08-08 16:50:15 +0800

    fix useApiClient

--- apps/web/utils/web-api.ts
+++ apps/web/utils/web-api.ts
@@ -8,7 +8,7 @@ export function useApiClient() {
   const { webUrl } = usePublicEnv();
   const [client] = useState(() =>
     initClient(contract, {
-      baseUrl: typeof window !== "undefined" ? `${webUrl}/api` : "/api",
+      baseUrl: typeof window === "undefined" ? `${webUrl}/api` : "/api",
     })
   );
 
