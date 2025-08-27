0c95141836a6f33f4c2649f8880f03b2f8e0e23d
mogita <me@mogita.com>
2025-08-27 16:50:46 +0800

    chore: add generated migration files

---
+++ packages/database/migrations/0008_fixed_deathbird.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `videos` ADD `duration` float;--> statement-breakpoint
+ALTER TABLE `videos` ADD `width` int;--> statement-breakpoint
+ALTER TABLE `videos` ADD `height` int;--> statement-breakpoint
+ALTER TABLE `videos` ADD `fps` int;
