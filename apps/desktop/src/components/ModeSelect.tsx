2da01f7adff1234982c9bee1656e92c182c7151c
Ameer Al Ashhab <33054370+ameer2468@users.noreply.github.com>
2025-08-29 17:22:39 +0300

    update new flow with mode tips (#970)

--- apps/desktop/src/components/ModeSelect.tsx
+++ apps/desktop/src/components/ModeSelect.tsx
@@ -1,5 +1,5 @@
 import { cx } from "cva";
-import type { JSX } from "solid-js";
+import { type JSX, Show } from "solid-js";
 import { createOptionsQuery } from "~/utils/queries";
 import type { RecordingMode } from "~/utils/tauri";
 
@@ -7,7 +7,8 @@ interface ModeOptionProps {
 	mode: RecordingMode;
 	title: string;
 	description: string;
-	icon: (props: { class: string; style: JSX.CSSProperties }) => JSX.Element;
+	standalone?: boolean;
+	icon: (props: { class: string; style?: JSX.CSSProperties }) => JSX.Element;
 	isSelected: boolean;
 	onSelect: (mode: RecordingMode) => void;
 }
@@ -17,26 +18,12 @@ const ModeOption = (props: ModeOptionProps) => {
 		<div
 			onClick={() => props.onSelect(props.mode)}
 			class={cx(
-				`p-4 space-y-3 rounded-lg bg-gray-2 transition-all duration-200`,
-				{
-					"ring-2 ring-offset-2 hover:bg-gray-2 cursor-default ring-blue-9 ring-offset-gray-100":
-						props.isSelected,
-					"ring-2 ring-transparent ring-offset-transparent hover:bg-gray-3 cursor-pointer":
-						!props.isSelected,
-				},
+				`p-5 space-y-3 rounded-lg border transition-all duration-200 border-gray-4 dark:border-gray-3 h-fit bg-gray-3 dark:bg-gray-2`,
 			)}
 		>
 			<div class="flex flex-col items-center mb-2 text-center">
 				{props.icon({
-					class: cx(
-						"size-12 mb-3",
-						props.isSelected ? "opacity-100" : "opacity-30",
-					),
-					style: {
-						filter: props.isSelected
-							? "drop-shadow(0 0 0.5rem rgba(255, 255, 255, 0.5))"
-							: "none",
-					},
+					class: "size-12 mb-5 invert dark:invert-0",
 				})}
 				<h3 class="text-lg font-medium text-gray-12">{props.title}</h3>
 			</div>
@@ -48,7 +35,7 @@ const ModeOption = (props: ModeOptionProps) => {
 	);
 };
 
-const ModeSelect = () => {
+const ModeSelect = (props: { onClose?: () => void; standalone?: boolean }) => {
 	const { rawOptions, setOptions } = createOptionsQuery();
 
 	const handleModeChange = (mode: RecordingMode) => {
@@ -73,7 +60,28 @@ const ModeSelect = () => {
 	];
 
 	return (
-		<div class="grid grid-cols-2 gap-8 text-center">
+		<div
+			class={cx(
+				"grid grid-cols-2 gap-8 items-center text-center bg-gray-1",
+				props.standalone
+					? "absolute z-10 border border-gray-3 p-16 rounded-xl"
+					: "",
+			)}
+			onClick={(e) => e.stopPropagation()}
+		>
+			<Show when={props.onClose}>
+				<div
+					onClick={() => props.onClose?.()}
+					class="absolute -top-2.5 -right-2.5 p-2 rounded-full border duration-200 bg-gray-2 border-gray-3 hover:bg-gray-3 transition-duration"
+				>
+					<IconCapX class="invert-1 size-2 dark:invert" />
+				</div>
+			</Show>
+			{props.standalone && (
+				<h2 class="text-[24px] col-span-2 font-medium text-center text-gray-12">
+					Recording Modes
+				</h2>
+			)}
 			{modeOptions.map((option) => (
 				<ModeOption
 					mode={option.mode}
