a9b760a6e7ca2fddd0e068e7334e10d9474f1e65
mogita <me@mogita.com>
2025-08-31 21:27:15 +0800

    chore: restore database files

--- packages/database/migrations/0008_fixed_deathbird.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `videos` ADD `duration` float;--> statement-breakpoint
-ALTER TABLE `videos` ADD `width` int;--> statement-breakpoint
-ALTER TABLE `videos` ADD `height` int;--> statement-breakpoint
-ALTER TABLE `videos` ADD `fps` int;
