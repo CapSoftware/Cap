0da715f76e824421e968c07f98cf1ee84070fe9e
Oscar Beaumont <oscar@otbeaumont.me>
2025-09-24 14:39:49 +0800

    nit

--- apps/web/app/api/thumbnail/route.ts
+++ apps/web/app/api/thumbnail/route.ts
@@ -37,7 +37,7 @@ export async function GET(request: NextRequest) {
 		return new Response(
 			JSON.stringify({ error: true, message: "Video not found" }),
 			{
-				status: 401,
+				status: 404,
 				headers: getHeaders(origin),
 			},
 		);
