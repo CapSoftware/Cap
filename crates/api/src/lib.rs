a782308a82fade29b8e89400d9ae9d3153013776
Oscar Beaumont <oscar@otbeaumont.me>
2025-10-02 16:00:56 +1000

    wip

--- crates/api/src/lib.rs
+++ crates/api/src/lib.rs
@@ -1,34 +1,3 @@
-///! Types and implements for the Cap web API endpoints.
+//! Types and implements for the Cap web API endpoints.
 
-pub struct UploadMultipartInitiate {
-    video_id: String,
-}
-
-// impl UploadMultipartInitiate {
-//     pub fn as_request(self) -> reqwest::Request {
-//         let mut request = reqwest::Request::new(
-//             reqwest::Method::POST,
-//             "https://api.example.com/upload_multipart_initiate"
-//                 .parse()
-//                 .unwrap(),
-//         );
-//         request.header("Authorization", "Bearer YOUR_TOKEN");
-//         request.json(&self).unwrap();
-//         request
-//     }
-// }
-
-// #[derive(Default)]
-// pub struct Api {
-//     client: reqwest::Client,
-//     bearer: Option<String>, // TODO: Hook this up
-// }
-
-// impl Api {
-//     pub fn upload_multipart_initiate(&self) {
-//         // self.client
-//         todo!();
-//     }
-// }
-
-// TODO: Helper for retries, exponential backoff
+// TODO: Migrate `apps/desktop/src-tauri/upload.rs` here once we figure out how auth will work with that.
