436a2b7a67904d1e369719f13f4c4d1d68b64e53
Richie McIlroy <33632126+richiemcilroy@users.noreply.github.com>
2025-10-08 19:05:03 +0100

    feat: web-domain bits

--- packages/web-domain/src/Rpcs.ts
+++ packages/web-domain/src/Rpcs.ts
@@ -1,6 +1,7 @@
 import { RpcGroup } from "@effect/rpc";
 
+import { AppsRpcs } from "./Apps.ts";
 import { FolderRpcs } from "./Folder.ts";
 import { VideoRpcs } from "./Video.ts";
 
-export const Rpcs = RpcGroup.make().merge(VideoRpcs, FolderRpcs);
+export const Rpcs = RpcGroup.make().merge(VideoRpcs, FolderRpcs, AppsRpcs);
