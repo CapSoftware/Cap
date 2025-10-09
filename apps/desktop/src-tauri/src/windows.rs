db748d0cdb102d788f2c012fbd1d165d434e63f9
Patrick Delorme <104777874+p-delorme@users.noreply.github.com>
2025-10-09 07:56:25 -0400

    Reduces initial camera window size. (#1154)

--- apps/desktop/src-tauri/src/windows.rs
+++ apps/desktop/src-tauri/src/windows.rs
@@ -168,7 +168,7 @@ impl CapWindowId {
             Self::Main => (300.0, 360.0),
             Self::Editor { .. } => (1275.0, 800.0),
             Self::Settings => (600.0, 450.0),
-            Self::Camera => (460.0, 920.0),
+            Self::Camera => (200.0, 200.0),
             Self::Upgrade => (950.0, 850.0),
             Self::ModeSelect => (900.0, 500.0),
             _ => return None,
