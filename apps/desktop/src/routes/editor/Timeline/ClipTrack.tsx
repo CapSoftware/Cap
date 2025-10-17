d4445e993acbea54bc376ee258b2aba8b9b05d41
Eneji Victor <enejivic@gmail.com>
2025-10-17 04:52:30 +0100

    fix(editor): show scissors cursor when clip tool is active

--- apps/desktop/src/routes/editor/Timeline/ClipTrack.tsx
+++ apps/desktop/src/routes/editor/Timeline/ClipTrack.tsx
@@ -195,6 +195,8 @@ export function ClipTrack(
 	const hasMultipleRecordingSegments = () =>
 		editorInstance.recordings.segments.length > 1;
 
+	const split = () => editorState.timeline.interactMode === "split";
+
 	return (
 		<TrackRoot ref={props.ref}>
 			<For each={segments()}>
@@ -351,6 +353,7 @@ export function ClipTrack(
 									isSelected()
 										? "wobble-wrapper border-gray-12"
 										: "border-transparent",
+										split() ? "timeline-scissors-cursor" : "",
 								)}
 								innerClass="ring-blue-9"
 								segment={relativeSegment}
@@ -406,6 +409,7 @@ export function ClipTrack(
 									onMouseDown={(downEvent) => {
 										const start = segment.start;
 
+										if(split()) return;
 										const maxSegmentDuration =
 											editorInstance.recordings.segments[
 												segment.recordingSegment ?? 0
@@ -496,6 +500,7 @@ export function ClipTrack(
 									onMouseDown={(downEvent) => {
 										const end = segment.end;
 
+										if(split()) return;
 										const maxSegmentDuration =
 											editorInstance.recordings.segments[
 												segment.recordingSegment ?? 0
