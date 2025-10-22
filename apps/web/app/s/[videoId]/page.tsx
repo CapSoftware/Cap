3e0b7035ed28f9fb4ab8a19a17e22468ae348c7d
Ameer Al Ashhab <33054370+ameer2468@users.noreply.github.com>
2025-10-23 00:43:42 +0300

    web: improve owner check (#1277)
    * improve check
    
    * naming
    
    * Update Summary.tsx
    
    * Update ShareHeader.tsx

--- apps/web/app/s/[videoId]/page.tsx
+++ apps/web/app/s/[videoId]/page.tsx
@@ -697,6 +697,7 @@ async function AuthorizedContent({
 		organizationMembers: membersList.map((member) => member.userId),
 		organizationId: video.sharedOrganization?.organizationId ?? undefined,
 		sharedOrganizations: sharedOrganizations,
+		ownerIsPro: video.ownerIsPro ?? false,
 		password: null,
 		folderId: null,
 		orgSettings: video.orgSettings || null,
@@ -728,6 +729,7 @@ async function AuthorizedContent({
 					data={videoWithOrganizationInfo}
 					videoSettings={videoWithOrganizationInfo.settings}
 					user={user}
+					ownerIsPro={videoWithOrganizationInfo.ownerIsPro}
 					comments={commentsPromise}
 					views={viewsPromise}
 					customDomain={customDomain}
