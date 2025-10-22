c45611b2dbb360bc0941524146ba21c7e322e388
ameer2468 <33054370+ameer2468@users.noreply.github.com>
2025-10-23 01:07:03 +0300

    fix border rounding of video share

--- apps/web/app/s/[videoId]/_components/CapVideoPlayer.tsx
+++ apps/web/app/s/[videoId]/_components/CapVideoPlayer.tsx
@@ -521,7 +521,7 @@ export function CapVideoPlayer({
 			)}
 			<div
 				className={clsx(
-					"flex absolute inset-0 z-10 justify-center items-center bg-black transition-opacity duration-300 overflow-visible",
+					"flex absolute inset-0 z-10 rounded-xl justify-center items-center bg-black transition-opacity duration-300 overflow-visible",
 					videoLoaded || !!uploadProgress
 						? "opacity-0 pointer-events-none"
 						: "opacity-100",
@@ -660,7 +660,7 @@ export function CapVideoPlayer({
 				mainControlsVisible={(arg: boolean) => setMainControlsVisible(arg)}
 				isUploadingOrFailed={isUploading || isUploadFailed}
 			>
-				<MediaPlayerControlsOverlay />
+				<MediaPlayerControlsOverlay className="rounded-b-xl" />
 				<MediaPlayerSeek
 					tooltipThumbnailSrc={
 						isMobile || !resolvedSrc.isSuccess
