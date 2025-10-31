96c004ee61ae2e9808886fc7f715de94fe1a9121
Eneji Victor <enejivic@gmail.com>
2025-10-31 15:33:28 +0100

    Fixed the accessibility issue

--- apps/web/components/theme-toggle-icon.tsx
+++ apps/web/components/theme-toggle-icon.tsx
@@ -6,18 +6,12 @@ export const ThemeToggleIcon = () => {
 	const { theme } = useTheme();
 
 	return (
-		<button
-			type="button"
-			className="view-transition-theme-icon"
-			aria-label={
-				theme === "dark" ? "Switch to light theme" : "Switch to dark theme"
-			}
-		>
+		<span className="view-transition-theme-icon">
 			{theme === "dark" ? (
 				<Moon size={17} className="fill-white stroke-gray-3" />
 			) : (
 				<Sun size={17} className="stroke-gray-12" />
 			)}
-		</button>
+		</span>
 	);
 };
