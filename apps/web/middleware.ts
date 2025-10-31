c5dc23197984004b45d59a64e958c95aad9205d2
Brendan Allan <brendonovich@outlook.com>
2025-10-31 21:00:13 +0800

    add /embed to allowed self hosting routes

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -41,6 +41,7 @@ export async function middleware(request: NextRequest) {
 		if (
 			!(
 				path.startsWith("/s/") ||
+				path.startsWith("/middleware") ||
 				path.startsWith("/dashboard") ||
 				path.startsWith("/onboarding") ||
 				path.startsWith("/api") ||
