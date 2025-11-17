3e8dbec3261df938422e6968a8c9d3a20b90a1bc
Richie McIlroy <33632126+richiemcilroy@users.noreply.github.com>
2025-11-17 00:19:39 +0000

    Fix window shadow on Windows platform

--- apps/desktop/src-tauri/src/windows.rs
+++ apps/desktop/src-tauri/src/windows.rs
@@ -636,7 +636,7 @@ impl ShowCapWindow {
                     .maximized(false)
                     .resizable(false)
                     .fullscreen(false)
-                    .shadow(true)
+                    .shadow(!cfg!(windows))
                     .always_on_top(true)
                     .transparent(true)
                     .visible_on_all_workspaces(true)
