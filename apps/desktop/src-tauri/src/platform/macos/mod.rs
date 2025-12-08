0ded5a982306294ade95eb5c82003cd6edcbc815
Ilya <47112191+ItsEeleeya@users.noreply.github.com>
2025-12-08 12:15:16 +0330

    Safety: Return Retained<NSWindow> instead of ref

--- apps/desktop/src-tauri/src/platform/macos/mod.rs
+++ apps/desktop/src-tauri/src/platform/macos/mod.rs
@@ -1,3 +1,4 @@
+use objc2::{Message, rc::Retained};
 use objc2_app_kit::{NSWindow, NSWindowButton};
 use tauri::WebviewWindow;
 
@@ -6,20 +7,21 @@ mod sc_shareable_content;
 pub use sc_shareable_content::*;
 
 pub trait WebviewWindowExt {
-    fn objc2_nswindow(&self) -> &NSWindow;
+    fn objc2_nswindow(&self) -> Retained<NSWindow>;
 
     fn set_traffic_lights_visible(&self, visible: bool);
 }
 
 impl WebviewWindowExt for WebviewWindow {
     #[inline]
-    fn objc2_nswindow(&self) -> &NSWindow {
+    fn objc2_nswindow(&self) -> Retained<NSWindow> {
         // SAFETY: This cast is safe as the existence of the WebviewWindow means it's attached to an NSWindow
         unsafe {
-            &*self
+            (&*self
                 .ns_window()
                 .expect("WebviewWindow is always backed by NSWindow")
-                .cast()
+                .cast::<NSWindow>())
+                .retain()
         }
     }
 
