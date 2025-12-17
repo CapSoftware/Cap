4b70ba83a90f3dceca676bef8abb58869f6b01b1
Richie McIlroy <33632126+richiemcilroy@users.noreply.github.com>
2025-12-17 22:49:39 +0000

    clippy

--- crates/scap-ffmpeg/examples/cli.rs
+++ crates/scap-ffmpeg/examples/cli.rs
@@ -3,33 +3,43 @@ pub async fn main() {
     #[cfg(windows)]
     {
         use scap_direct3d::*;
+        use scap_targets::Display;
+        use std::time::Duration;
 
-        let display = Display::primary().unwrap();
+        let display = Display::primary();
 
-        let capturer = Capturer::new(
-            display.try_as_capture_item().unwrap(),
+        let mut capturer = Capturer::new(
+            display.raw_handle().try_as_capture_item().unwrap(),
             Settings {
                 is_border_required: Some(true),
                 is_cursor_capture_enabled: Some(true),
                 pixel_format: PixelFormat::R8G8B8A8Unorm,
+                ..Default::default()
             },
-        );
-
-        let capture_handle = capturer
-            .start(|frame| {
+            |frame| {
                 use scap_ffmpeg::AsFFmpeg;
 
                 let ff_frame = frame.as_ffmpeg()?;
 
-                dbg!(ff_frame.width(), ff_frame.height(), ff_frame.format());
+                println!(
+                    "Frame: {}x{} format={:?}",
+                    ff_frame.width(),
+                    ff_frame.height(),
+                    ff_frame.format()
+                );
 
                 Ok(())
-            })
-            .unwrap();
+            },
+            || Ok(()),
+            None,
+        )
+        .unwrap();
+
+        capturer.start().unwrap();
 
         std::thread::sleep(Duration::from_secs(3));
 
-        capture_handle.stop().unwrap();
+        capturer.stop().unwrap();
 
         std::thread::sleep(Duration::from_secs(3));
     }
