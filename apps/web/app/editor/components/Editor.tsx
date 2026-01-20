363581a46bf343b2bc66192bcda3eb4aab0ece9c
Richie McIlroy <33632126+richiemcilroy@users.noreply.github.com>
2026-01-20 23:36:54 +0000

    feat: add editor Editor component
    Main composition component that brings together Header, Player, ConfigSidebar, and Timeline with EditorProvider context and keyboard shortcuts.

---
+++ apps/web/app/editor/components/Editor.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import type { ProjectConfiguration } from "../types/project-config";
+import { ConfigSidebar } from "./ConfigSidebar";
+import { EditorProvider } from "./context";
+import { Header } from "./Header";
+import { Player } from "./Player";
+import { Timeline } from "./Timeline";
+import { useEditorShortcuts } from "./useEditorShortcuts";
+
+interface VideoData {
+	id: string;
+	name: string;
+	duration: number;
+	width: number;
+	height: number;
+}
+
+interface EditorProps {
+	video: VideoData;
+	videoUrl: string;
+	initialConfig?: ProjectConfiguration;
+}
+
+function EditorContent({ videoId }: { videoId: string }) {
+	useEditorShortcuts();
+
+	return (
+		<div className="flex flex-col h-screen bg-gray-3 overflow-hidden">
+			<Header videoId={videoId} />
+			<div className="flex flex-1 gap-3 p-3 min-h-0">
+				<div className="flex flex-col flex-1 gap-3 min-h-0 min-w-0">
+					<Player />
+					<div className="h-40 shrink-0">
+						<Timeline />
+					</div>
+				</div>
+				<ConfigSidebar />
+			</div>
+		</div>
+	);
+}
+
+export function Editor({ video, videoUrl, initialConfig }: EditorProps) {
+	return (
+		<EditorProvider
+			video={video}
+			videoUrl={videoUrl}
+			initialConfig={initialConfig}
+		>
+			<EditorContent videoId={video.id} />
+		</EditorProvider>
+	);
+}
