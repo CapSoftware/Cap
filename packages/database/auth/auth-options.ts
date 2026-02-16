4b6a7187e1d02f8f7cd39cb316bf9e2ccfe16fb6
Richie McIlroy <33632126+richiemcilroy@users.noreply.github.com>
2026-02-16 15:52:33 +0000

    Only query DB in jwt callback when needed

--- packages/database/auth/auth-options.ts
+++ packages/database/auth/auth-options.ts
@@ -162,26 +162,36 @@ export const authOptions = (): NextAuthOptions => {
 				return session;
 			},
 			async jwt({ token, user }) {
-				const [dbUser] = await db()
-					.select()
-					.from(users)
-					.where(eq(users.email, token.email || ""))
-					.limit(1);
+				if (user || !token.id) {
+					const [dbUser] = await db()
+						.select({
+							id: users.id,
+							name: users.name,
+							lastName: users.lastName,
+							email: users.email,
+							image: users.image,
+						})
+						.from(users)
+						.where(eq(users.email, token.email || ""))
+						.limit(1);
 
-				if (!dbUser) {
-					if (user) {
-						token.id = user?.id;
+					if (!dbUser) {
+						if (user) {
+							token.id = user?.id;
+						}
+						return token;
 					}
-					return token;
+
+					return {
+						id: dbUser.id,
+						name: dbUser.name,
+						lastName: dbUser.lastName,
+						email: dbUser.email,
+						picture: dbUser.image,
+					};
 				}
 
-				return {
-					id: dbUser.id,
-					name: dbUser.name,
-					lastName: dbUser.lastName,
-					email: dbUser.email,
-					picture: dbUser.image,
-				};
+				return token;
 			},
 		},
 	};
