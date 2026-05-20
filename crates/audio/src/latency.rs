123071b113d49f1b56550273b88bfc27e428905e
Richie McIlroy <33632126+richiemcilroy@users.noreply.github.com>
2026-05-20 21:06:57 +0100

    build(workspace): rename unchecked_duration_subtraction lint

--- crates/audio/src/latency.rs
+++ crates/audio/src/latency.rs
@@ -941,7 +941,7 @@ mod windows {
 }
 
 #[cfg(test)]
-#[allow(clippy::unchecked_time_subtraction)]
+#[allow(clippy::unchecked_duration_subtraction)]
 mod tests {
     use super::*;
     use std::time::Instant;
